0e3206c21e1200ab3b080f27dc9d5db06631de36
MohIceAge <mohamed.hadjoudj@protonmail.com>
2017-04-06 17:07:52 +0200

    init webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require ("path")
+const webpack = require ("webpack")
+
+module.exports = {
+    entry: path.join (__dirname, "src", "scripts", "main.js"),
+    output: {
+        path: path.join (__dirname, "src", "dist"),
+        filename: "main.js",
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+            },
+        ],
+    },
+}
